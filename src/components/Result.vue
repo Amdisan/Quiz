bd1b139a52d275e74ac4276b6d3d0695821332d1
Amdisan <amdisan1@gmail.com>
2023-12-03 22:06:56 +0200

    final

---
+++ src/components/Result.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="result">
+    <div class="title">{{ results[resultIdx].title }}</div>
+    <div class="desc">{{ results[resultIdx].desc }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["results", "totalCorrect"],
+  computed: {
+    resultIdx() {
+      let idx = 0;
+      this.results.forEach((el, i) => {
+        if (el.min <= this.totalCorrect && el.max >= this.totalCorrect) {
+          idx = i;
+        }
+      });
+      return idx;
+    },
+  },
+};
+</script>
